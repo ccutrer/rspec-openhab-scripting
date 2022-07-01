f674671c058843410e55f22d8edec104616e4471
Cody Cutrer <cody@cutrer.us>
2022-07-01 11:30:44 -0600

    ensure groups don't trigger rules when resetting spec state

--- lib/rspec/openhab/state.rb
+++ lib/rspec/openhab/state.rb
@@ -1,9 +1,24 @@
 # frozen_string_literal: true
 
 RSpec.configure do |config|
+  org.openhab.core.items.GenericItem.field_reader :eventPublisher
+
+  # rubocop:disable Style/GlobalVars
   config.before(:each) do
-    $ir.for_each do |_provider, item| # rubocop:disable Style/GlobalVars
+    ep = $ir.eventPublisher
+
+    # stash event publishers to avoid triggering any rules
+    $ir.for_each do |_provider, item|
+      item.event_publisher = nil
+    end
+
+    $ir.for_each do |_provider, item| # rubocop:disable Style/CombinableLoops
       item.state = NULL # don't use update, to avoid triggering any rules
     end
+  ensure
+    $ir.for_each do |_provider, item|
+      item.event_publisher = ep
+    end
   end
+  # rubocop:enable Style/GlobalVars
 end
