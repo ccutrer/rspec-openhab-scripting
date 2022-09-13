c068ce327b50c925640b1ca3153d8f86683c3258
Cody Cutrer <cody@cutrer.us>
2022-09-13 10:28:07 -0600

    ensure the correct version of jruby is running
    unfortunately rubygems doesn't have a way to specify engine version
    requirements

--- lib/rspec-openhab-scripting.rb
+++ lib/rspec-openhab-scripting.rb
@@ -2,6 +2,11 @@
 
 require "openhab/version"
 
+unless RUBY_ENGINE == "jruby" &&
+       Gem::Version.new(RUBY_ENGINE_VERSION) >= Gem::Version.new("9.3.8.0")
+  raise Gem::RubyVersionMismatch, "rspec-openhab-scripting requires JRuby 9.3.8.0 or newer"
+end
+
 # we completely override some files from openhab-scripting
 $LOAD_PATH.unshift("#{__dir__}/rspec")
 
